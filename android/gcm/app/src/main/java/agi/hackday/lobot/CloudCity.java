eed2485565c82419396425642c5fafa02228e8eb
Louis Trempe <ltrempe@ag.com>
2016-02-25 11:15:09 -0500

    Successful registration with cloud city

---
+++ android/gcm/app/src/main/java/agi/hackday/lobot/CloudCity.java
@@ -0,0 +1,13 @@
+package agi.hackday.lobot;
+
+/**
+ * Created by ltrempe on 2/25/16.
+ */
+public class CloudCity {
+    public enum Status {
+        CONNECTED,
+        DISCONNECTED;
+    }
+    public static final String REGIRATION_COMPLETE = "registrationComplete";
+    public static final String EXTRA_REGISTRATION_STATUS= "cc_extra_registration_status";
+}
